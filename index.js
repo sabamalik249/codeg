87921b01d0c37e83ad8c6fd099440f5a0bae7adb
sabamalik249 <sabamalik249@gmail.com>
2021-11-09 12:34:36 +0530

    controllers+routes+views

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const app=express();
 
 const port= 8000;
 
+app.use('/',require('./routes'));
+
+app.set('view engine','ejs');
+app.set('views','./views');
+
 app.listen(port,function(err){
     if(err){
         console.log("error",err);
