19bf23e4b178a7c8c428abf8b3a5da57d172036c
sabamalik249 <sabamalik249@gmail.com>
2021-11-09 16:43:19 +0530

    sign-up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,3 +1,44 @@
+const User = require("../models/user");
+
 module.exports.profile=function(req,res){
-    return res.end('<h1>PROFILE PAGE</h1>');
+    return res.render('user_profile',{
+        title:' PROFILE'
+    });
+}
+
+module.exports.signIn=function(req,res){
+    return res.render('user_sign_in',{
+        title: 'SIGN-IN'
+    });
+}
+
+module.exports.signUp = function (req, res) {
+    return res.render('user_sign_up', {
+        title: 'SIGN-UP'
+    });
+}
+
+
+//get the sign up data
+module.exports.create=function(req,res){
+    if(req.body.password != req.body.confirm_password){
+        return res.redirect('back');
+    }
+    User.findOne({email: req.body.email},function(err,user){
+        if(err){console.log('error in finding user in signing up'); return}
+
+        if(!user){
+            User.create(req.body ,function(err,user){
+                if(err){console.log("error in creating the user while signing up") ;return}
+
+                return res.redirect('/users/sign-in');
+            })
+        }else{
+            return res.redirect('back');
+        }
+    })
+}
+
+module.exports.createSession = function (req, res) {
+//to do
 }
--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express= require('express');
+const cookieParser=require('cookie-parser');
 const app=express();
-
+const expressLayouts = require('express-ejs-layouts');
 const port= 8000;
+const db = require('./config/mongoose');
+
+
+app.use(express.urlencoded());
+
+app.use(cookieParser());
+app.use(express.static('./assets'));
+
+app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/',require('./routes'));
 
 app.set('view engine','ejs');
 app.set('views','./views');
+
 
 app.listen(port,function(err){
     if(err){
